ef8d303c5541bd1f30e255537163f7d339e5a23e
AmnaAli45 <anasahmed.ru6@gmail.com>
2025-03-08 11:24:17 +0500

    counting odd and even numbers in an array sequentially

---
+++ sequential.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define SIZE 1000000 // defining the size of array 
+
+//creating a function to count even and odd numbers in a given array. This function executes sequentially.
+
+void countEvenOddSequential(int arr[], int size) {
+    int even = 0, odd = 0; // variables to store even and odd count of an array
+    
+    //whenever an odd number comes in array it will incement even otherwise odd will be incremented.
+    //this loop will execute until whole array is explored.
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 == 0) 
+            even++;
+        else
+            odd++;
+    }
+
+    
+    printf("Sequential Execution:\nEven Numbers are: %d\nOdd Numbers are: %d\n", even, odd);
+    
+}
+
+int main() {
+    int arr[SIZE]; //declaring an array of definesd size
+    
+    // Initialize array with random numbers
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 1000;  // it will generate random numbers between0 and 999
+    }
+
+    printf("\n===...... Counting Even and Odd Numbers ....===\n");
+    countEvenOddSequential(arr, SIZE); 
+    
+    
+
+    return 0;
+}
+
